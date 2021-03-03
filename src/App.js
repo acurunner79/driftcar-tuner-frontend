f81f069ca3211da15c5198c82d744a672f115b5a
Jorge Soto <djsmoothzouk@gmail.com>
2021-03-03 13:31:41 -0600

    initial commit for dev deployment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to</h1>
+        <h1>Driftcar Tuner Garage</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Coming soon to a garage near you!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
